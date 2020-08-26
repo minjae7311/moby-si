0f0cebd6cad45a8e3256042b62e2e0369cdf94ec
alswnd <alswnd0427@gmail.com>
2020-08-27 00:11:48 +0900

    form submit with input id, pw

--- src/Routes/Login/LoginContainer.tsx
+++ src/Routes/Login/LoginContainer.tsx
@@ -2,30 +2,47 @@
 
 import React from "react";
 import LoginPresenter from "./LoginPresenter";
+import { RouteComponentProps } from "react-router-dom";
 
-class LoginContainer extends React.Component {
-  constructor(props) {
+interface IState {
+  inputId: string;
+  inputPw: string;
+}
+
+interface IProps extends RouteComponentProps<any> {
+  // 이게 있어야 readonly 에러 안난다.
+  location: any;
+  history: any;
+}
+
+class LoginContainer extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
     super(props);
-    console.log(props);
+    this.state = { inputId: "", inputPw: "" };
 
-    this.state = {};
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  public onInputChange: React.ChangeEventHandler<HTMLInputElement> = (
-    event
-  ) => {
-    console.log(event);
+  public onChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    this.setState({ [name]: value } as any);
   };
 
-  public onClick: React.ChangeEventHandler<HTMLButtonElement> = (event) => {
-    console.log(event);
+  public onSubmit: React.ChangeEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    console.log(this.state);
   };
 
   public render() {
     return (
       <LoginPresenter
-        onChange={this.onInputChange}
-        onClick={this.onClick}
+        idValue={this.state.inputId}
+        pwValue={this.state.inputPw}
+        onChange={this.onChange}
+        onSubmit={this.onSubmit}
       ></LoginPresenter>
     );
   }
--- src/Routes/Login/LoginPresenter.tsx
+++ src/Routes/Login/LoginPresenter.tsx
@@ -1,29 +1,27 @@
 /** @format */
 
 import * as React from "react";
-import LoginInput from "../../Components/LoginInput/LoginInput";
-import LoginButton from "../../Components/LoginButton/LoginButton";
-import styled from "../../typed-components";
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-`;
+import LoginForm from "../../Components/LoginForm/LoginForm";
 
 interface IProps {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  onClick: (event: React.ChangeEvent<HTMLButtonElement>) => void;
+  onSubmit: (event: React.ChangeEvent<HTMLFormElement>) => void;
+  idValue: string;
+  pwValue: string;
 }
 
-const LoginPresenter: React.SFC<IProps> = ({ onChange, onClick }) => (
-  <Container>
-    <LoginInput placeholder={"ID"} onChange={onChange}></LoginInput>
-    <LoginInput placeholder={"PW"} onChange={onChange}></LoginInput>
-    <LoginButton onClick={onClick}></LoginButton>
-  </Container>
+const LoginPresenter: React.SFC<IProps> = ({
+  onChange,
+  onSubmit,
+  idValue,
+  pwValue,
+}) => (
+  <LoginForm
+    onChange={onChange}
+    onSubmit={onSubmit}
+    idValue={idValue}
+    pwValue={pwValue}
+  ></LoginForm>
 );
 
 export default LoginPresenter;
