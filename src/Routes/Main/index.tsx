3276b878f0eb3acd5e0b418e777fad4a1c13487a
alswnd <alswnd0427@gmail.com>
2020-09-01 22:31:39 +0900

    admin resolver tested

--- src/Routes/Main/index.tsx
+++ src/Routes/Main/index.tsx
@@ -1,11 +1,28 @@
 /** @format */
 import * as React from "react";
 import LogoutButton from "../../Components/LogoutButton/LogoutButton";
+import { useMutation } from "@apollo/client";
+import { aminResolverTest_AdminResolverTest } from "../../types/api";
+import { ADMIN_RESOLVER_TEST } from "../../Routes/Main/mutation.gql";
 
 export const Main: React.SFC = () => {
+  const [adminResolverTest] = useMutation<aminResolverTest_AdminResolverTest>(
+    ADMIN_RESOLVER_TEST,
+    {
+      onCompleted: () => {
+        console.log("completed");
+      },
+    }
+  );
   return (
     <div>
-      <h1>Hi</h1>
+      <h1
+        onClick={async () => {
+          await adminResolverTest();
+        }}
+      >
+        Hi
+      </h1>
       <LogoutButton />
     </div>
   );
