d6da2ade2fd04ddfafda5e0a663ea6145cf24420
alswnd <alswnd0427@gmail.com>
2020-09-02 22:10:32 +0900

    a

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -2,26 +2,17 @@
 
 import * as React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import { AdminLogin } from "./Login";
+// import { AdminLogin } from "./Login";
 import { SIForm } from "./SIForm";
 
-const token = localStorage.getItem("jwt");
-
-const LoggedIn: React.FC = () => (
-  <Switch>
-    <Route path={"/"} exact={true} component={SIForm} />
-  </Switch>
-);
-
-const LoggedOut: React.FC = () => (
-  <Switch>
-    <Route path={"/"} exact={true} component={AdminLogin} />
-    <Redirect path="*" to="/" />
-  </Switch>
-);
-
 const Root: React.FC = () => (
-  <BrowserRouter>{token ? <LoggedIn /> : <LoggedOut />}</BrowserRouter>
+  <BrowserRouter>
+    <Switch>
+      {/* <Route path="/" exact component={AdminLogin} /> */}
+      <Route path="/SIForm" exact component={SIForm} />
+      <Redirect path="*" to="/" />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Root;
