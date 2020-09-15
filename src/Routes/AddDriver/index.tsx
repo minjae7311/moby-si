7e0da59787a9568be28d75712ab38c2875dcc3f2
alswnd <alswnd0427@gmail.com>
2020-09-15 21:19:17 +0900

    add driver

--- src/Routes/AddDriver/index.tsx
+++ src/Routes/AddDriver/index.tsx
@@ -1,5 +1,78 @@
 import * as React from "react";
+import { SIForm } from "../SIForm";
+import { DetailContainer } from "../../Components/Container/Container";
+import { useState } from "react";
+
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { useHistory } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { ADD_DRIVER } from "./mutation.gql";
+import { goDetail } from "../../Functions/functions";
 
 export const AddDriver: React.SFC = () => {
-	return <div>hi</div>;
+	const [driverData, setDriverData] = useState({
+		lat: "",
+		lng: "",
+		loginId: "",
+		loginPw: "",
+		privateTaxi: "",
+		company: "",
+		driveLicenseNumber: "",
+		taxiLicenseNumber: "",
+		fullName: "",
+		profilePhotoUrl: "",
+		phoneNumber: "",
+		gender: "",
+		accepted: "",
+		birthDate: "",
+	});
+
+	const [addDriver] = useMutation(ADD_DRIVER, { variables: { ...driverData } });
+
+	const history = useHistory();
+
+	const onChange = (event, header) => {
+		setDriverData({ ...driverData, [header]: event.target.value });
+	};
+
+	const addThisDriver = async () => {
+		await addDriver().then((result) => {
+			console.log(result);
+			const { data } = result;
+			goDetail(history, "driver", data.AddDriver.driver.id);
+		});
+	};
+
+	return (
+		<SIForm>
+			<DetailContainer>
+				<Form onSubmit={(e) => e.preventDefault()}>
+					{Object.keys(driverData).map((key, index) => {
+						return (
+							<Form.Group as={Row} controlId="formHorizontalEmail" key={index}>
+								<Form.Label column sm={2}>
+									{key}
+								</Form.Label>
+								<Col sm={10}>
+									<Form.Control
+										onChange={(e) => {
+											onChange(e, key);
+										}}
+										value={driverData[key]}
+									/>
+								</Col>
+							</Form.Group>
+						);
+					})}
+
+					<Button style={{ width: "100%" }} variant="outline-primary" onClick={addThisDriver}>
+						등록하기
+					</Button>
+				</Form>
+			</DetailContainer>
+		</SIForm>
+	);
 };
---
+++ src/Routes/AddDriver/mutation.gql.ts
@@ -0,0 +1,43 @@
+import { gql } from "@apollo/client";
+
+export const ADD_DRIVER = gql`
+	mutation addDriver(
+		$lat: Int
+		$lng: Int
+		$loginId: String
+		$loginPw: String
+		$privateTaxi: Boolean
+		$company: String
+		$driveLicenseNumber: String
+		$taxiLicenseNumber: String
+		$fullName: String
+		$profilePhotoUrl: String
+		$phoneNumber: String
+		$gender: Boolean
+		$accepted: Boolean
+		$birthDate: String
+	) {
+		AddDriver(
+			lat: $lat
+			lng: $lng
+			loginId: $loginId
+			loginPw: $loginPw
+			privateTaxi: $privateTaxi
+			company: $company
+			driveLicenseNumber: $driveLicenseNumber
+			taxiLicenseNumber: $taxiLicenseNumber
+			fullName: $fullName
+			profilePhotoUrl: $profilePhotoUrl
+			phoneNumber: $phoneNumber
+			gender: $gender
+			accepted: $accepted
+			birthDate: $birthDate
+		) {
+			ok
+			error
+			driver {
+				id
+			}
+		}
+	}
+`;
